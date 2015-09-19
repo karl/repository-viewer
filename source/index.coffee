1a972e9ea5833d06dd6f279d69e6cc09b21361b4
Karl O'Keeffe <karl@monket.net>
2015-09-19 21:32:46 +0100

    More Webpack Goodness!
    * Notifications on builds/failures
    * Auto reload page on any changes
    * Require .less/.css files and build into a separate .css file
    * Lint .coffee files
    * Require .coffee files without needing an extension 

--- source/index.coffee
+++ source/index.coffee
@@ -1,3 +1,5 @@
+require './index.less'
+
 repos = []
 
 $.ajax
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,34 @@
+var WebpackNotifierPlugin = require('webpack-notifier');
+var LiveReloadPlugin = require('webpack-livereload-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = {
-  entry: './source/index.coffee',
+  entry: './source',
   output: {
-    path: './dist/scripts',
+    path: './dist/assets',
     filename: 'index.js'
   },
   module: {
+    preLoaders: [
+      {
+        test: /\.coffee$/,
+        exclude: /node_modules/,
+        loader: "coffeelint-loader"
+      }
+    ],
     loaders: [
-      { test: /\.coffee$/, loader: "coffee-loader" }
+      { test: /\.coffee$/, loader: "coffee-loader" },
+      { test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader") },
+      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") },
+      { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' } // inline base64 URLs for <=8k images, direct URLs for the rest
     ]
-  }
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.coffee']
+  },
+  plugins: [
+    new WebpackNotifierPlugin(),
+    new LiveReloadPlugin(),
+    new ExtractTextPlugin("styles.css")
+  ]
 };
